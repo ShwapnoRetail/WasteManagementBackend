06070bebafde843e8fecf2b7cd3343ff620f660a
tanvirUpol <tanvirrahmanupol@gmail.com>
2023-06-03 02:50:58 +0600

    update

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const User = require('../models/UserModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+   return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+}
+
+// login user
+const loginUser = async (req, res) => {
+    const {staffId, password} = req.body
+    
+    try { 
+        const user = await User.login(staffId, password)
+        // create a token
+        const token = createToken(user._id)
+
+        const role = user.role.toLowerCase()
+        
+ 
+        res.status(200).json({staffId, token, role})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    } 
+}
+
+// signup user
+const signupUser = async (req, res) => {
+
+    const {staffId,staffName,password,outletCode,role} = req.body
+
+    try { 
+        const user = await User.signup(staffId,staffName,password,outletCode,role)
+
+        
+
+        // create a token
+        const token = createToken(user._id)
+
+        // const role = user.role.toLowerCase()
+ 
+        res.status(200).json({staffId, token, role})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }  
+ }
+
+ module.exports = {
+     loginUser,
+     signupUser
+ }
---
+++ models/UserModel.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+
+const Schema = mongoose.Schema
+
+
+const UserSchema = new Schema({
+    staffId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    staffName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    outletCode: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+})
+
+UserSchema.statics.signup = async function( staffId,staffName,  password,outletCode,role ){
+
+        const exists = await this.findOne({ staffId })
+
+        if(exists){
+            throw new Error('This user already exists')
+        }
+
+        if(!validator.isStrongPassword(password)) {
+            throw new Error('Password not strong enough')
+        }
+    
+
+        
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // console.log(hashedPassword)
+
+
+        
+        if(!exists){
+            const user = await this.create({ staffId,staffName, password: hashedPassword,outletCode,role })
+            return user
+        } 
+        
+    }
+
+
+    // static login method
+
+UserSchema.statics.login = async function(staffId, password) {
+
+    if(staffId && password){
+        
+        const user = await this.findOne({ staffId })
+
+        if(!user) {
+            throw new Error('User does not exist')
+        }
+
+        const valid = await bcrypt.compare(password, user.password)
+
+        if(!valid) {
+            throw new Error('Incorrect password')
+        }
+
+        return user
+    }
+    if (!staffId && !password) {
+            throw new Error('ID and password are required')
+    }
+    
+    if (staffId || !password) {
+        throw new Error('Password is required')
+    }
+    
+    if (!staffId || password) {
+        throw new Error('ID is required')
+    }
+    
+    
+
+}
+
+module.exports = mongoose.model('User', UserSchema)
