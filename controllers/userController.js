f822999da567b3923ebc264f58de2bc2312030df
tanvirUpol <tanvirrahmanupol@gmail.com>
2023-06-13 01:23:20 +0600

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/UserModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-   return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+   return jwt.sign({_id}, process.env.SECRET, {expiresIn: '9999 years'})
 }
 
 // login user
